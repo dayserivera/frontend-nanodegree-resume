4b376bc7f88b007328ece0ee5093b39142c69356
dayserivera <dayserivera@gmail.com>
2016-10-10 17:59:57 -0300

    comitei

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var formattedName = HTMLheaderName.replace("%data%", "Dayse Aparecida Rivera Rocha");
+ var role = HTMLheaderRole.replace("%data%", "Web Developer");
+  /*$('#header').prepend(role);
+  $('#header').prepend(formattedName);
+ */
+ var bio = {
+	 "name":"Dayse Aparecida Rivera Rocha",
+	 "role":"Web Developer",
+	 "contactInfo":"(11)983059390",
+	 "pictureUrl":"me.png",
+	 "welcomeMessagem":"Hola!",
+	 "skills":["Java Programmer", "English Speaker"]
+ };
+ $('#header').prepend(bio.role);
+ $('#header').prepend(bio.name);
+ 
