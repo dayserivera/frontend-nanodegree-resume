4ec25f8f5ada6612fa3b81b15630c24bd52aaf55
daparecidar <daparecidar@indracompany.com>
2016-10-16 17:31:12 -0200

    commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,19 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var formattedName = HTMLheaderName.replace("%data%", "Dayse Aparecida Rivera Rocha");
- var role = HTMLheaderRole.replace("%data%", "Web Developer");
-  /*$('#header').prepend(role);
-  $('#header').prepend(formattedName);
- */
+
  var bio = {
 	 "name":"Dayse Aparecida Rivera Rocha",
 	 "role":"Web Developer",
-	 "contactInfo":"(11)983059390",
-	 "pictureUrl":"me.png",
+	 "contactInfo":{"mobile":"(11)983059390", "facebook":"dayserivera", "github":"dayserivera"},
+	 "pictureUrl":"images/me.png",
 	 "welcomeMessagem":"Hola!",
 	 "skills":["Java Programmer", "English Speaker"]
  };
- $('#header').prepend(bio.role);
- $('#header').prepend(bio.name);
+ var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var role = HTMLheaderRole.replace("%data%", bio.role);
+ 
+ $('#header').prepend(role);
+  $('#header').prepend(formattedName);
  
