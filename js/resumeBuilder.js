f76e38a22a749df1eff227f1e91ce2b76e34365b
dayserivera <dayserivera@gmail.com>
2016-10-17 12:01:16 -0200

    novas atualizacoes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,24 +1,62 @@
-var cameron = {};
-cameron.job = "course dev";
-
-var makeCourse = function(){
-	console.log("Made a course");
-}
-
-var courses = 0;
-while(cameron.job === "course dev"){
-	makeCourse();
-	courses = courses + 1;
-	if(courses === 10){
-		cameron.job = "learning specialist";
+var work = {
+    "jobs":  [
+        {
+            "employer": "International Bank",
+            "title": "Unix engineer",
+            "location": "London, UK",
+            "dates": "2004 to current",
+            "description": "Derivative trading support"
+        },
+        {
+            "employer": "International BankG",
+            "title": "Unix SA",
+            "location": "Frankfurt, Germany",
+            "dates": "2000 to 2004",
+            "description": "AIX, Solaris and Linux sysadmin"
+        },
+        {
+            "employer": "Dutch ISP",
+            "title": "Unix SA",
+            "location": "Amsterdam, Netherlands",
+            "dates": "1998 to 2000",
+            "description": "Supporting the ISP Unix systems"
+        },
+        {
+            "employer": "Clearing House",
+            "title": "Unix engineer",
+            "location": "Luxembourg, Luxembourg",
+            "dates": "1998 to 1998",
+            "description": "Real-time settlement system"
+        },
+        {
+            "employer": "Local bank",
+            "title": "Unix SA",
+            "location": "Florence, Italy",
+            "dates": "1994 to 1998",
+            "description": "Various projects"
+        },
+        {
+            "employer": "Multiple clients and employers",
+            "title": "Multiple roles",
+            "location": "Perugia, Italy",
+            "dates": "1990 to 1994",
+            "description": "Multiple locations from Perugia to London"
+        }
+    ]
+};
+function displayWork(){
+	for(var job in work.jobs){	
+		$('#workExperience').append(HTMLworkStart);
+		var formattedJob = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedJobTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedJobDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedJobDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		
+		$('.work-entry:last').append(formattedJob + formattedJobTitle + formattedJobDates + formattedJobDescription); 	
 	}
 }
 
-console.log(cameron.job);
-
-for(var i = 0; i < 9; i++){
-	console.log(i);
-}
+displayWork();
 
 /*
 This is empty on purpose! Your code to build the resume will go here.
